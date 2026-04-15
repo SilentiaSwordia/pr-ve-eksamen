fe737d2a3e4331b2e682df0215631f79328b7d54
SilentiaSwordia <joachim.rl08@gmail.com>
2026-04-15 11:24:49 +0200

    Implement modal functionality for services section and update IDs for list items

---
+++ script.js
@@ -0,0 +1,45 @@
+const openBTNTransport = document.getElementById("openTransport");
+const openBTNOptimalisering = document.getElementById("openOptimalisering");
+const openBTNLøsninger = document.getElementById("openLøsninger");
+const modalTransport = document.getElementById("transport");
+const modalOptimalisering = document.getElementById("optimalisering");
+const modalLøsninger = document.getElementById("løsninger");
+const closeBTNTransport = document.getElementById("closeTransport");
+const closeBTNOptimalisering = document.getElementById("closeOptimalisering");
+const closeBTNLøsninger = document.getElementById("closeLøsninger");
+
+if (openBTNTransport) {
+  openBTNTransport.addEventListener("click", () => {
+    modalTransport.classList.add("open");
+  });
+}
+
+if (openBTNOptimalisering) {
+  openBTNOptimalisering.addEventListener("click", () => {
+    modalOptimalisering.classList.add("open");
+  });
+}
+
+if (openBTNLøsninger) {
+  openBTNLøsninger.addEventListener("click", () => {
+    modalLøsninger.classList.add("open");
+  });
+}
+
+if (closeBTNTransport) {
+  closeBTNTransport.addEventListener("click", () => {
+    modalTransport.classList.remove("open");
+  });
+}
+
+if (closeBTNOptimalisering) {
+  closeBTNOptimalisering.addEventListener("click", () => {
+    modalOptimalisering.classList.remove("open");
+  });
+}
+
+if (closeBTNLøsninger) {
+  closeBTNLøsninger.addEventListener("click", () => {
+    modalLøsninger.classList.remove("open");
+  });
+}
